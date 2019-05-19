090ffbfb7b85a94d338fcd8ef1031b5dd5ebbd3c
entorius <eligijus.puplinskas@gmail.com>
2019-05-20 01:33:15 +0300

    basic stuff finally set

--- SocialVU/App_Start/RouteConfig.cs
+++ SocialVU/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SocialVU
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { action = "Index", id = UrlParameter.Optional }
+                defaults: new {controller="default", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
