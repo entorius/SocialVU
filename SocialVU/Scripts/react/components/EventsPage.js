77d49a28953320269212353a11cd935cc8ad6c2d
Gabija <gabija.savu@gmail.com>
2019-06-03 15:47:57 +0300

    new pages

---
+++ SocialVU/Scripts/react/components/EventsPage.js
@@ -0,0 +1,116 @@
+﻿import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CalenderIcon from '@material-ui/icons/CalendarToday';
+import LocationIcon from '@material-ui/icons/LocationOn';
+import Divider from '@material-ui/core/Divider';
+
+
+const styles = theme => ({
+    root: {
+        marginLeft: '5%',
+        marginRight: '5%',
+        marginTop: theme.spacing(3),
+    },
+    card: {
+        width: 300,
+        height: 250,
+        marginBottom: theme.spacing(3)
+
+    },
+    title: {
+        fontSize: 20,
+        textAlign: 'center',
+        textcolor: '#d32f2f',
+        marginBottom: theme.spacing(3)
+    },
+    icon: {
+        marginRight: theme.spacing(1),
+        color: '#9e9e9e',
+
+    },
+    text: {
+        fontSize: 10,
+    },
+    decriptionText: {
+        fontSize: 12,
+    },
+    divider: {
+        marginTop: theme.spacing(1),
+        marginBottom: theme.spacing(1)
+    },
+    pageTitle: {
+        marginBottom: theme.spacing(3),
+    },
+
+});
+
+class EventsPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            events: [
+                { title: "testas", description: "aprašymas", date: "2019-05-29", time: "18:00", location: "Vilnius University" },
+                { title: "testas", description: "aprašymas", date: "2019-05-29", time: "18:00", location: "Vilnius University" },
+                { title: "testas", description: "aprašymas", date: "2019-05-29", time: "18:00", location: "Vilnius University" },
+                { title: "testas", description: "aprašymas", date: "2019-05-29", time: "18:00", location: "Vilnius University" },
+                { title: "testas", description: "aprašymas", date: "2019-05-29", time: "18:00", location: "Vilnius University" },
+                { title: "testas", description: "aprašymas", date: "2019-05-29", time: "18:00", location: "Vilnius University" },
+                { title: "testas", description: "aprašymas", date: "2019-05-29", time: "18:00", location: "Vilnius University" },
+                { title: "testas", description: "aprašymas", date: "2019-05-29", time: "18:00", location: "Vilnius University" },
+            ],
+            openDialog: false,
+            dialogIndex: 0
+        }
+    }
+
+    render() {
+        const { classes } = this.props
+        return (
+
+            <div className={classes.root}>
+
+                <Typography color="secondary" variant="h3" className={classes.pageTitle}>Events</Typography >
+                <Grid container spacing={24}>
+                    {this.state.events.map((event, index) =>
+                        <Grid item xs={4}>
+                            <Card className={classes.card} key={index}>
+                                <CardContent>
+                                    <Typography className={classes.title} color="secondary" >
+                                        {event.title}
+                                    </Typography>
+                                    <Grid
+                                        container
+                                        direction="row"
+                                    >
+                                        <CalenderIcon className={classes.icon} />
+                                        <Typography className={classes.text} color="primary" >Data: {event.date} {event.time}</Typography>
+                                    </Grid>
+                                    <Grid
+                                        container
+                                        direction="row"
+                                    >
+                                        <LocationIcon className={classes.icon} />
+                                        <Typography className={classes.text} color="primary"> Vieta: {event.location} </Typography>
+                                    </Grid>
+                                    <Divider className={classes.divider} />
+                                    <Typography className={classes.decriptionText}>
+                                        {event.description}
+                                    </Typography>
+                                </CardContent>
+
+                            </Card>
+                        </Grid>)}
+                </Grid>
+            </div>
+
+        )
+    }
+
+}
+
+
+export default withStyles(styles)(EventsPage)
---
+++ SocialVU/Scripts/react/components/NewsPage.js
@@ -0,0 +1,83 @@
+﻿import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+
+const styles = theme => ({
+    root: {
+        marginLeft: '5%',
+        marginRight: '5%',
+        marginTop: theme.spacing(3),
+    },
+    card: {
+        minWidth: 275,
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pageTitle: {
+        marginBottom: theme.spacing(3),
+    },
+    dialog: {
+        width: "600px",
+        height: "600px"
+    },
+});
+
+class MainPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            news: [
+                { title: "testas", description: "aprašymas", date: "2019-05-29" },
+                { title: "testas", description: "aprašymas", date: "2019-05-29" },
+                { title: "testas", description: "aprašymas", date: "2019-05-29" },
+                { title: "testas", description: "aprašymas", date: "2019-05-29" },
+            ]
+        }
+    }
+
+    render() {
+        const { classes } = this.props
+        return (
+
+            <div className={classes.root}>
+
+                <Typography color="secondary" variant="h3" className={classes.pageTitle}>News</Typography >
+                <div>
+                    {this.state.news.map((n, index) =>
+                        <Card className={classes.card} key={index} >
+                            <CardActionArea >
+                                <CardContent>
+                                    <Typography color="secondary" variant="h5" >
+                                        {n.title}
+                                    </Typography>
+                                    <Typography className={classes.pos} >
+                                        {n.date}
+                                    </Typography>
+                                    <Typography variant="body1">
+                                        {n.description}
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+
+                    )}
+                </div>
+            </div>
+        )
+    }
+
+}
+
+
+export default withStyles(styles)(MainPage)
